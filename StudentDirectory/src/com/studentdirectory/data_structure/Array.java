58c331d3d13072e388e529fd7c831c0ce633764b
Nithin <nithin@Nithins-MacBook-Air.local>
2026-09-03 13:11:03 -0700

    Earliest Moment When Everyone Become Friends

--- StudentDirectory/src/com/studentdirectory/data_structure/Array.java
+++ StudentDirectory/src/com/studentdirectory/data_structure/Array.java
@@ -1,7 +1,9 @@
 package com.studentdirectory.data_structure;
+import java.util.Arrays;
 
 public class Array
 {
+    // MAX volume of water
     public static int maxArea(int[] height) {
         //mininum of 2 integers = min
         //difference between 2 indices = diff 
@@ -36,9 +38,55 @@ public class Array
         return max;
     }
 
+    //Earliest Moment When Everyone Become Friends
+    public static int earliestAcq(int[][] logs, int n) {
+        Arrays.sort(logs, (a, b) -> Integer.compare(a[0], b[0]));
+        int count = n;
+        for(int[] log:logs)
+        {
+            int time = log[0];
+            int person1 = log[1];
+            int person2 = log[2];
+            
+            if(person1 != person2)
+            {
+                logs = update(logs,person1,person2);
+                count--;
+                if(count == 1)
+                {
+                    return time;
+                }
+            }
+        }
+        return -1;
+    }
+    public static int[][] update(int[][] logs,int person1, int person2)
+    {
+        for(int[] log:logs)
+        {
+            if(log[1]==person1)
+            {
+                log[1]=person2;
+            }
+            if(log[2]==person1)
+            {
+                log[2]=person2;
+            }
+        }
+        return logs;
+    }
+
     public static void main(String str[])
     {
         int[] waterArray = {1,8,6,2,5,4,8,3,7};
         System.out.println("Maximum wanter volume = "+maxArea(waterArray));
+
+        int[][] matrix = {
+            {1, 0, 1},
+            {2, 1, 2},
+            {3, 0, 2},
+            {4, 2, 3}
+        };
+        System.out.println("Earliest Time Everyone Became Friends = "+earliestAcq(matrix,4));
     }
 }
